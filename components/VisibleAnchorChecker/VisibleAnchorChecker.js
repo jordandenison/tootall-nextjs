e21711f5d5faec1b8cf967d68fce3982dd26325e
Jordan Denison <jordan@denisonweb.com>
2019-07-09 21:18:21 +0000

    fix VisibleAnchorChecker logic

--- components/VisibleAnchorChecker/VisibleAnchorChecker.js
+++ components/VisibleAnchorChecker/VisibleAnchorChecker.js
@@ -10,10 +10,10 @@ const checkIfElementInViewport = el => {
   const rect = el.getBoundingClientRect()
 
   return typeof window !== 'undefined' && typeof document !== 'undefined' ? (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    rect.bottom > 0 &&
+    rect.right > 0 &&
+    rect.left < (window.innerWidth || document.documentElement.clientWidth),
+    rect.top < (window.innerHeight || document.documentElement.clientHeight)
   ) : false
 }
 
